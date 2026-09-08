2efe643fc8faf7089eaf37724e018f235a04679e
Doreen <doreenlinda@users.noreply.github.com>
2026-09-08 20:45:05 +0200

    SABOTAGE - wird zurueckgenommen

--- app/src/main/java/com/jarvis/app/WakeWordService.kt
+++ app/src/main/java/com/jarvis/app/WakeWordService.kt
@@ -938,7 +938,7 @@ class WakeWordService : Service() {
      * ABSICHT steckt im Fokus-Antrag und bleibt davon unberuehrt.
      */
     private fun weckerAusgang(): android.media.AudioAttributes {
-        val usage = if (ueberBluetooth()) {
+        val usage = if (false) {
             android.media.AudioAttributes.USAGE_MEDIA
         } else {
             android.media.AudioAttributes.USAGE_ALARM
